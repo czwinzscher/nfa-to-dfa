406102fa7320cc2a4b3d86fc15c005403495d9fb
clemens <czwinzscher2@gmail.com>
2020-02-18 00:22:36 +0100

    use try to handle readFile exceptions

--- app/Main.hs
+++ app/Main.hs
@@ -2,16 +2,18 @@
 
 module Main where
 
-import Control.Monad.Extra (unlessM)
+import Control.Exception
 import Data.Aeson (eitherDecode)
 import qualified Data.ByteString.Lazy as B
 import NfaToDfa
-import System.Directory (doesFileExist)
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
 
 nfaFromJSONFile :: String -> IO (Either String NFA)
-nfaFromJSONFile filename = eitherDecode <$> B.readFile filename
+nfaFromJSONFile filename =
+  (try $ B.readFile filename) >>= \case
+    Left err -> return $ Left (show (err :: SomeException))
+    Right content -> return $ eitherDecode content
 
 main :: IO ()
 main =
@@ -19,8 +21,7 @@ main =
     [] ->
       putStrLn "please provide the name of the json file as an argument" >>
       exitFailure
-    (f:_) -> do
-      unlessM (doesFileExist f) $ putStrLn "file does not exist" >> exitFailure
+    (f:_) ->
       nfaFromJSONFile f >>= \case
         Left errorStr -> putStrLn ("Error: " ++ errorStr) >> exitFailure
         Right nfa -> print $ removeUnreachable (nfaToDfa nfa)
