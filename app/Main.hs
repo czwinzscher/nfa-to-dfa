7b4ba942f91d3c2fab20bc82591bd44fa821489f
clemens <czwinzscher2@gmail.com>
2020-02-17 23:26:10 +0100

    check if file exists

--- app/Main.hs
+++ app/Main.hs
@@ -2,18 +2,25 @@
 
 module Main where
 
+import Control.Monad.Extra (unlessM)
 import Data.Aeson (eitherDecode)
 import qualified Data.ByteString.Lazy as B
 import NfaToDfa
+import System.Directory (doesFileExist)
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
 
 nfaFromJSONFile :: String -> IO (Either String NFA)
-nfaFromJSONFile filename = B.readFile filename >>= return . eitherDecode
+nfaFromJSONFile filename = eitherDecode <$> B.readFile filename
 
 main :: IO ()
-main = do
-  [filename] <- getArgs
-  nfaFromJSONFile filename >>= \case
-    Left errorStr -> putStrLn ("Error: " ++ errorStr) >> exitFailure
-    Right nfa -> print $ removeUnreachable (nfaToDfa nfa)
+main =
+  getArgs >>= \case
+    [] ->
+      putStrLn "please provide the name of the json file as an argument" >>
+      exitFailure
+    (f:_) -> do
+      unlessM (doesFileExist f) $ putStrLn "file does not exist" >> exitFailure
+      nfaFromJSONFile f >>= \case
+        Left errorStr -> putStrLn ("Error: " ++ errorStr) >> exitFailure
+        Right nfa -> print $ removeUnreachable (nfaToDfa nfa)
