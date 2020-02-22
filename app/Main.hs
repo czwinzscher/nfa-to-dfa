4d8b4288ab6924d6a401c6b21b7a8b26c3d08219
clemens <czwinzscher2@gmail.com>
2020-02-22 15:58:20 +0100

    use optparse-applicative for option parsing

--- app/Main.hs
+++ app/Main.hs
@@ -4,24 +4,49 @@
 module Main where
 
 import Control.Exception
-import Data.Aeson (eitherDecode)
+import Data.Aeson (eitherDecode, encode)
 import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString.Lazy.Char8 as BC
 import NfaToDfa
-import System.Environment (getArgs)
+import Options.Applicative
 import System.Exit (exitFailure)
+
+data Opts =
+  Opts
+    { jsonFile :: String
+    , outputJson :: Bool
+    }
+
+optsParser :: Parser Opts
+optsParser =
+  Opts <$>
+  strOption
+    (long "filename" <>
+     short 'f' <> metavar "FILENAME" <> help "json file with a NFA") <*>
+  switch (long "output-json" <> help "print the DFA in json format")
 
 nfaFromJSONFile :: String -> IO (Either String NFA)
 nfaFromJSONFile filename =
   (eitherDecode <$> B.readFile filename) `catch` \(e :: IOException) ->
     return $ Left (show e)
 
+run :: Opts -> IO ()
+run (Opts f j) =
+  nfaFromJSONFile f >>= \case
+    Left errorStr -> putStrLn ("Error: " ++ errorStr) >> exitFailure
+    Right nfa -> do
+      let res = removeUnreachable (nfaToDfa nfa)
+      if j
+        then BC.putStrLn $ encode res
+        else print res
+
+infoStr :: ParserInfo Opts
+infoStr =
+  info
+    (optsParser <**> helper)
+    (fullDesc <>
+     progDesc "Convert a NFA into a DFA" <>
+     header "nfa-to-dfa - a nfa to dfa converter")
+
 main :: IO ()
-main =
-  getArgs >>= \case
-    [] ->
-      putStrLn "please provide the name of the json file as an argument" >>
-      exitFailure
-    (f:_) ->
-      nfaFromJSONFile f >>= \case
-        Left errorStr -> putStrLn ("Error: " ++ errorStr) >> exitFailure
-        Right nfa -> print $ removeUnreachable (nfaToDfa nfa)
+main = run =<< execParser infoStr
