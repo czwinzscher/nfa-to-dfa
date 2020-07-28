27a0fdebf5bce4ae76f82aa221fb2e9fedd1c4d1
clemens <czwinzscher2@gmail.com>
2020-07-28 11:31:56 +0200

    add option to read from stdin and to write to stdout

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,6 @@
 
 module Main where
 
-import Control.Exception
 import Data.Aeson (eitherDecode)
 import Data.Aeson.Encode.Pretty (encodePretty)
 import qualified Data.ByteString.Lazy as B
@@ -12,42 +11,68 @@ import NfaToDfa
 import Options.Applicative
 import System.Exit (exitFailure)
 
-data Opts =
-  Opts
-    { jsonFile :: String
-    , outputJson :: Bool
-    }
+data Input = FileInput FilePath | StdInput
+
+data Output = JsonOutput | TextOutput
+
+data Opts = Opts
+  { inputSource :: Input,
+    outputFormat :: Output,
+    outputFile :: Maybe String
+  }
 
 optsParser :: Parser Opts
 optsParser =
-  Opts <$>
-  strOption
-    (long "filename" <>
-     short 'f' <> metavar "FILENAME" <> help "json file with a NFA") <*>
-  switch (long "output-json" <> help "print the DFA in json format")
+  Opts
+    <$> ( FileInput
+            <$> strOption
+              ( long "file"
+                  <> short 'f'
+                  <> metavar "FILENAME"
+                  <> help "input file"
+              )
+            <|> flag' StdInput (long "stdin" <> help "read from stdin")
+        )
+    <*> ( flag' JsonOutput (long "output-json" <> help "output the DFA in JSON-Format")
+            <|> flag' TextOutput (long "output-text" <> help "output the DFA in Text-Format")
+        )
+    <*> ( optional $
+            strOption
+              ( long "output-file"
+                  <> short 'o'
+                  <> metavar "FILENAME"
+                  <> help "output file"
+              )
+        )
 
-nfaFromJSONFile :: String -> IO (Either String NFA)
-nfaFromJSONFile filename =
-  (eitherDecode <$> B.readFile filename) `catch` \(e :: IOException) ->
-    return $ Left (show e)
-
-run :: Opts -> IO ()
-run (Opts f j) =
-  nfaFromJSONFile f >>= \case
-    Left errorStr -> putStrLn ("Error: " ++ errorStr) >> exitFailure
-    Right nfa -> do
-      let res = removeUnreachable (nfaToDfa nfa)
-      if j
-        then BC.putStrLn $ encodePretty res
-        else print res
+nfaFromJSONFile :: B.ByteString -> Either String NFA
+nfaFromJSONFile json = eitherDecode json
 
 infoStr :: ParserInfo Opts
 infoStr =
   info
     (optsParser <**> helper)
-    (fullDesc <>
-     progDesc "Convert a NFA into a DFA" <>
-     header "nfa-to-dfa - a nfa to dfa converter")
+    ( fullDesc
+        <> progDesc "Convert a NFA into a DFA"
+        <> header "nfa-to-dfa - a nfa to dfa converter"
+    )
+
+run :: Opts -> IO ()
+run (Opts inSource outFormat outFile) = do
+  json <- case inSource of
+    FileInput filename -> B.readFile filename
+    StdInput -> B.getContents
+  case eitherDecode json of
+    Left errorStr -> putStrLn ("Error: " ++ errorStr) >> exitFailure
+    Right nfa ->
+      let dfa = removeUnreachable $ nfaToDfa nfa
+          res = case outFormat of
+            JsonOutput -> encodePretty dfa
+            TextOutput -> BC.pack $ show dfa
+          outputFunc = case outFile of
+            Just f -> BC.writeFile f
+            Nothing -> BC.putStrLn
+       in outputFunc res
 
 main :: IO ()
-main = run =<< execParser infoStr
+main = execParser infoStr >>= run
